a36c66cff6feb5138f46f4d5d629bf0bf1026d90
yang2110862 <1563782600@qq.com>
2021-06-17 15:36:57 +0800

    修改变量名

--- player/sdk/code/source/data.cpp
+++ player/sdk/code/source/data.cpp
@@ -1,30 +1,30 @@
 #include "data.h"
 
-unordered_map<int, ListNode*> numToSatellite;
+unordered_map<int, ListNode*> numToEntity;
 vector<int> base_station;
 void Init::Creat_map(uint32_t N) {
     for (int i = 0; i < N; ++i) {      
         ListNode* node = new ListNode;
         node->num = i;
-        numToSatellite[i] = node;
+        numToEntity[i] = node;
     }
 }
 
 void Init::Init_list(uint32_t N, uint32_t E, const vector<bool>& typeVec, const vector<Edge>& edgeVec) {
     Creat_map(N);
     for (int i = 0; i < N; ++i) {
-        numToSatellite[i]->isSatellite = typeVec[i];
+        numToEntity[i]->isSatellite = typeVec[i];
         if (!typeVec[i]) base_station.push_back(i);
     }
     for (int i = 0; i < E; ++i) {
         int send = edgeVec[i].send;
         int recv = edgeVec[i].recv;
         int dist = edgeVec[i].dist;
-        numToSatellite[send]->dist[recv] = dist;
-        numToSatellite[recv]->dist[send] = dist;
+        numToEntity[send]->dist[recv] = dist;
+        numToEntity[recv]->dist[send] = dist;
 
-        numToSatellite[send]->next.emplace_back(numToSatellite[recv]);
-        numToSatellite[recv]->next.emplace_back(numToSatellite[send]);
+        numToEntity[send]->next.emplace_back(numToEntity[recv]);
+        numToEntity[recv]->next.emplace_back(numToEntity[send]);
     }
 }
 
@@ -36,7 +36,7 @@ vector<Route> Init::Process(uint32_t N, uint32_t C, uint32_t D, uint32_t PS,
     for (int i = 0; i < base_station.size(); ++i) {
         int min_dist = INT_MAX;
         int num;
-        auto base = numToSatellite[base_station[i]];
+        auto base = numToEntity[base_station[i]];
         for (auto x : base->dist) {
             if (x.second < min_dist) {
                 min_dist = x.second;
@@ -44,7 +44,7 @@ vector<Route> Init::Process(uint32_t N, uint32_t C, uint32_t D, uint32_t PS,
             }
         }
         Route temp = {base_station[i], num};
-        // Route temp = {base_station[i], numToSatellite[base_station[i]]->next[0]->num};
+        // Route temp = {base_station[i], numToEntity[base_station[i]]->next[0]->num};
         sol.emplace_back(temp);
     }
     return sol;
